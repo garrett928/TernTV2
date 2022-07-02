263bd68f3e313866364660294b5cb21b6514943c
garrett <garrett7621@gmail.com>
2022-07-02 15:02:39 -0400

    display tern_pass on first slide

--- TernTV2/routes.py
+++ TernTV2/routes.py
@@ -1,7 +1,9 @@
 """Route declaration."""
+import imp
 from flask import current_app as app
 from flask import render_template
 from TernTV2.tern_helpers.status_grabber import tern_status
+from TernTV2.tern_helpers import passwd_grabber
 
 
 @app.route("/")
@@ -13,9 +15,15 @@ def home():
         description="Smarter page templates with Flask & Jinja."
     )
 
+"""
+This block of app routes is for the individual webpages. For example, the daily password page,
+daily quotes page, etc
+"""
+
 # show the intern status messages
 @app.route("/tern_status")
 def status_page():
+    # only here for testing
     status = {
         "Garrett" : "Rock Sleeper",
         "Tessa" : "Concusion Getter",
@@ -26,13 +34,27 @@ def status_page():
         status=tern_status()
     )
 
+# show the intern status messages
+@app.route("/tern_pass")
+def tern_pass():
+    return render_template(
+        'tern_pass.html',
+        passwd=passwd_grabber.tern_pass()
+    )
+
+"""
+This block of routes is the for slides pages. Slides are made up of one or more individual pages 
+and will be what is shown on the TV.
+"""
+
+
     # show the intern status messages
 @app.route("/slide_1")
 def slide_1():
     return render_template(
         '/slides/slide_2_x_2.html',
         frame_1="http://127.0.0.1:5000/tern_status",
-        frame_2="http://127.0.0.1:5000/tern_status",
-        frame_3="http://127.0.0.1:5000/tern_status",
+        frame_2="http://127.0.0.1:5000/tern_pass",
+        frame_3="http://127.0.0.1:5000/tern_pass",
         frame_4="http://127.0.0.1:5000/tern_status"
         )
